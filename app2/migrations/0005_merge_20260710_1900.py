94f60fdd7d9f7c90419ff1486873952f5320a718
bhargav983 <ssy.balu@gmail.com>
2026-07-10 20:57:10 +0530

    added inventoty module - menu item styling added

---
+++ app2/migrations/0005_merge_20260710_1900.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('app2', '0003_kot_served_by'),
+        ('app2', '0004_alter_stockmovement_movement_at'),
+    ]
+
+    operations = []
